839c45129cef1d393265a662ea6bd527c3d4a192
Tim <32385143+arcticstormx@users.noreply.github.com>
2020-06-20 07:02:36 +0300

    Complete form data submission process, for reset, error and success popups

--- js/form.js
+++ js/form.js
@@ -6,6 +6,7 @@
   roomsToCapacityValidation();
   typeToPriceValidation();
   timeinToTimeoutValidation();
+  formSubmission();
 
   // validate that guests number conforms to rooms capacity
   function roomsToCapacityValidation() {
@@ -118,20 +119,70 @@
     });
   }
 
+  // form submission process
+  function formSubmission() {
+    var form = document.querySelector(".ad-form");
+    form.addEventListener('submit', function(evt) {
+      window.upload(new FormData(form), onSuccess, onError);
+      evt.preventDefault();
+    });
+    var resetBtn = document.querySelector('.ad-form__reset');
+    resetBtn.addEventListener('click', function(evt) {
+      evt.preventDefault();
+      form.reset();
+      // add address value based on main pin
+      var mainPin = document.querySelector('.map__pin--main');
+      var address = document.querySelector('#address');
+      address.value = Math.round(mainPin.offsetLeft + mainPin.offsetWidth / 2) + ', ' + Math.round(mainPin.offsetTop + mainPin.offsetHeight + 22);
+    });
+    function onError(message) {
+      var frag = document.createDocumentFragment();
+      var temp = document.querySelector('#error').content;
+      var errorMsg = temp.cloneNode(true);
+      frag.appendChild(errorMsg);
+      // can't add event listeners straight to cloned node for some reason
+      // need to append it first
+      var newMsg = frag.querySelector('.error');
+      // event listeners to close message
+      newMsg.addEventListener("click", onMouseClick);
+      document.addEventListener("keydown", onEscPress);
 
-  var form = document.querySelector(".ad-form");
-  var onError = function (message) {
-    console.log(message);
-  };
-  var onSuccess = function (message) {
-    window.util.resetPage();
-    var main = document.querySelector('main');
-    var temp = document.querySelector('#success').content;
-    var successMsg = temp.cloneNode(true);
-    main.appendChild(successMsg);
-  };
-  form.addEventListener('submit', function(evt) {
-    window.upload(new FormData(form), onSuccess, onError);
-    evt.preventDefault();
-  });
+      var main = document.querySelector('main');
+      main.appendChild(frag);
+
+      function onMouseClick(evt) {
+        newMsg.remove();
+        document.removeEventListener("keydown", onEscPress);
+      }
+      function onEscPress(evt) {
+        if (evt.keyCode === 27) newMsg.remove();
+        document.removeEventListener("keydown", onEscPress);
+      }
+    };
+    function onSuccess(message) {
+      // reset page to inactive state
+      window.util.resetPage();
+      var frag = document.createDocumentFragment();
+      var temp = document.querySelector('#success').content;
+      var successMsg = temp.cloneNode(true);
+      frag.appendChild(successMsg);
+      // can't add event listeners straight to cloned node for some reason
+      // need to append it first
+      var newMsg = frag.querySelector('.success');
+      // event listeners to close message
+      newMsg.addEventListener("click", onMouseClick);
+      document.addEventListener("keydown", onEscPress);
+
+      var main = document.querySelector('main');
+      main.appendChild(frag);
+      function onMouseClick(evt) {
+        newMsg.remove();
+        document.removeEventListener("keydown", onEscPress);
+      }
+      function onEscPress(evt) {
+        if (evt.keyCode === 27) newMsg.remove();
+        document.removeEventListener("keydown", onEscPress);
+      }
+    };
+  }
 })();
